e34db71f783e01980c9eb1cc82433b6b765781c4
doja <srbh.jo314@gmail.com>
2026-03-23 23:28:05 +0530

    March23

--- app/storageModule/KVs.go
+++ app/storageModule/KVs.go
@@ -18,30 +18,113 @@ type NewEntry struct {
 }
 
 type KeyValue struct {
-	store   map[string]Entry
+	store   map[string]NewEntry
 	myLocks sync.RWMutex
 }
 
 var KV KeyValue = KeyValue{
-	store: make(map[string]Entry),
+	store: make(map[string]NewEntry),
 }
 
-func (Entry *NewEntry) LPush(value string) {
-	existingList, _ := Entry.value.([]string)
+func LPush(key string, insertionString string) error {
 
-	arr := []string{value}
-	arr = append(arr, existingList...)
-	Entry.value = arr
-}
+	KV.myLocks.Lock()
+	defer KV.myLocks.Unlock()
+	_, ok := KV.store[key]
+	if !ok {
+		KV.store[key] = NewEntry{value: []string{insertionString}}
+		return nil
+	}
+	switch val := KV.store[key].value.(type) {
+	case []string:
+		arr := []string{insertionString}
+		arr = append(arr, val...)
+		KV.store[key] = NewEntry{value: arr}
+		return nil
 
-func (Entry *NewEntry) RPush(value string) {
-	existingList, ok := Entry.value.([]string)
-	if ok != true {
-		Entry.value = []string{}
+	default:
+		return fmt.Errorf("Type of Value is not []string ")
 	}
 
-	existingList = append(existingList, value)
-	Entry.value = existingList
+}
+
+func RPush(key string, insertionString string) error {
+
+	KV.myLocks.Lock()
+	defer KV.myLocks.Unlock()
+	_, ok := KV.store[key]
+	if ok != true {
+		KV.store[key] = NewEntry{
+			value: []string{insertionString},
+		}
+		return nil
+	}
+	switch val := KV.store[key].value.(type) {
+	case []string:
+		val = append(val, insertionString)
+		KV.store[key] = NewEntry{value: val}
+	default:
+		return fmt.Errorf("Type of Value for Key Not Equal to []string")
+	}
+	return nil
+}
+
+// Command	What it does
+// LPUSH key val	Push to head
+// RPUSH key val	Push to tail
+// LPOP key	Remove + return from head
+// RPOP key	Remove + return from tail
+// // LRANGE key start stop	Get elements start..stop (0-indexed, -1 = last)
+// LLEN key
+
+func LPOP(key string) (string, error) {
+
+	KV.myLocks.Lock()
+	defer KV.myLocks.Unlock()
+	_, ok := KV.store[key]
+	if !ok {
+		KV.store[key] = NewEntry{value: []string{}}
+		return "", nil
+	}
+
+	switch val := KV.store[key].value.(type) {
+	case []string:
+		if len(val) == 0 {
+			KV.store[key] = NewEntry{value: []string{}}
+			return "", nil
+		}
+		f := val[0]
+		KV.store[key] = NewEntry{value: val[1:]}
+		return f, nil
+	default:
+		return "", fmt.Errorf("Type is NOT []string")
+	}
+
+}
+
+func RPOP(key string) (string, error) {
+
+	KV.myLocks.Lock()
+	defer KV.myLocks.Unlock()
+	_, ok := KV.store[key]
+	if !ok {
+		KV.store[key] = NewEntry{value: []string{}}
+		return "", nil
+	}
+
+	switch val := KV.store[key].value.(type) {
+	case []string:
+		if len(val) == 0 {
+			KV.store[key] = NewEntry{value: []string{}}
+			return "", nil
+		}
+		f := val[len(val)-1]
+		KV.store[key] = NewEntry{value: val[:len(val)-1]}
+		return f, nil
+	default:
+		return "", fmt.Errorf("Type is NOT []string")
+	}
+
 }
 
 func Set(KeyValList []string) error {
@@ -52,22 +135,23 @@ func Set(KeyValList []string) error {
 	func() {
 		KV.myLocks.Lock()
 		defer KV.myLocks.Unlock()
+		if len(KeyValList) == 3 {
+			KV.store[KeyValList[1]] = NewEntry{value: KeyValList[2]}
+			return
+		}
+
 		var timeToExpire time.Time
 		timeVal, err := strconv.Atoi(KeyValList[4])
 		if err != nil {
 			return
 		}
-		if len(KeyValList) == 3 {
-			KV.store[KeyValList[1]] = Entry{value: KeyValList[2]}
-		}
-
 		if KeyValList[3] == "PX" {
 			timeToExpire = time.Now().Add(time.Millisecond * time.Duration(timeVal))
 		} else {
 			timeToExpire = time.Now().Add(time.Second * time.Duration(timeVal))
 		}
 
-		KV.store[KeyValList[1]] = Entry{
+		KV.store[KeyValList[1]] = NewEntry{
 			value:  KeyValList[2],
 			expiry: timeToExpire,
 		}
@@ -81,12 +165,15 @@ func GET(key string) (error, string) {
 		return fmt.Errorf("KEY DOES NOT EXIST"), ""
 	}
 
-	if KV.store[key].expiry.IsZero() {
-		return nil, KV.store[key].value
-	}
-	if time.Now().After(v.expiry) == true {
-		return fmt.Errorf("TIME ELAPSED, KEY EXPIRED"), ""
+	if KV.store[key].expiry.IsZero() || time.Now().After(v.expiry) == false {
+		switch val := KV.store[key].value.(type) {
+		case string:
+			return nil, val
+		default:
+			return fmt.Errorf("WRONGTYPE Operation against a key holding the wrong kind of value"), ""
+
+		}
 	}
 
-	return nil, (KV.store[key].value)
+	return fmt.Errorf(" Key Expired "), ""
 }

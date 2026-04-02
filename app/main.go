c47b5c60a00baeec244d69b0a213a812b5f84bc6
Srbh117 <srbh.jo314@gmail.com>
2026-04-03 00:44:11 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
@@ -8,6 +9,8 @@ import (
 
 var _ = net.Listen
 var _ = os.Exit
+
+var SEP = "\n"
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
@@ -17,14 +20,20 @@ func main() {
 		return
 	}
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			os.Exit(1)
-		}
+	conn, err := l.Accept()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	buff := make([]byte, 1024)
+	conn.Read(buff)
+	curr_idx := 0
+	for bytes.Index(buff[curr_idx:], []byte(SEP)) != -1 {
 		_, err = conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
 			os.Exit(1)
 		}
+		curr_idx = bytes.Index(buff[curr_idx:], []byte(SEP))
 	}
+
 }

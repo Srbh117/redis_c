9789c8d9957cbc57ecca43eec354dca624a2a69e
Srbh117 <srbh.jo314@gmail.com>
2026-04-03 00:32:03 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,38 +6,37 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment the code below to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
-
-	conn, err := l.Accept()
 	if err != nil {
 		return
 	}
-
 	buff := make([]byte, 1024)
-	n, err := conn.Read(buff)
-	if err != nil {
-		return
-	}
 
-	fmt.Println("32: ", string(buff[:n]))
-	conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return
+		}
+
+		_, err = conn.Read(buff)
+		if err != nil {
+			return
+		}
+		conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			fmt.Println("Failed to bind to port 6379")
+			os.Exit(1)
+		}
+		_, err = l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
 }

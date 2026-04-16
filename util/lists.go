422cb0e70e1950caee5318d9733ad8a8b46af80b
Srbh117 <srbh.jo314@gmail.com>
2026-04-16 12:57:08 +0530

    codecrafters submit [skip ci]

--- util/lists.go
+++ util/lists.go
@@ -1,6 +1,6 @@
 package util
 
-var store map[string][]string
+var store map[string][]string = make(map[string][]string)
 
 func RPUSH(key string, val string) int {
 	store[key] = append(store[key], val)

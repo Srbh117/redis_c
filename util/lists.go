ce4ff576e2f7cdd453d5381e6fec14dc409a4566
Srbh117 <srbh.jo314@gmail.com>
2026-04-16 14:02:42 +0530

    codecrafters submit [skip ci]

--- util/lists.go
+++ util/lists.go
@@ -7,11 +7,32 @@ func RPUSH(key string, val []string) int {
 	return len(store[key])
 }
 
+func abs(x int) int {
+	if x < 0 {
+		return x * -1
+	}
+	return x
+}
+
 func LRANGE(key string, start, stop int) string {
 	val, ok := store[key]
 	if ok != true {
 		return ParseList([]string{})
 	}
+
+	if start < 0 {
+		if abs(start) > len(store[key]) {
+			start = 0
+		}
+		start += len(store[key])
+	}
+	if stop < 0 {
+		if abs(stop) > len(store[key]) {
+			stop = 0
+		}
+		stop += len(store[key])
+	}
+
 	if start >= len(val) {
 		return ParseList([]string{})
 	}
